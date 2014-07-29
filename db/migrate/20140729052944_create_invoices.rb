88693c629b42476dbcad84f1ab9c216111c04d28
Sachin Singh <singhsachin87@yahoo.com>
2014-07-29 07:46:26 +0200

    migration files updated

--- db/migrate/20140729052944_create_invoices.rb
+++ db/migrate/20140729052944_create_invoices.rb
@@ -2,10 +2,10 @@ class CreateInvoices < ActiveRecord::Migration
   def change
     create_table :invoices do |t|
       t.string :summary
-      t.string :invoice_from
-      t.string :date
+      t.string :from
+      t.date :date
       t.string :invoice_number
-      t.string :invoice_to
+      t.string :to
       t.string :invoice_due
       t.string :purchase_order_number
       t.text :invoice_notes
--- db/migrate/20140729054033_create_bills.rb
+++ db/migrate/20140729054033_create_bills.rb
@@ -2,11 +2,11 @@ class CreateBills < ActiveRecord::Migration
   def change
     create_table :bills do |t|
       t.string :summary
-      t.string :bill_from
-      t.string :date
+      t.string :from
+      t.date :date
       t.string :bill_number
-      t.string :bill_to
-      t.date :due_on
+      t.string :to
+      t.string :due_on
       t.string :purchase_order_number
       t.text :invoice_notes
 
