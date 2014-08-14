0911cac472a401e5816275bf6a450a67a0882ee9
Sachin Singh <singhsachin87@yahoo.com>
2014-08-14 13:04:06 +0200

    estimate model updated

--- app/models/estimate.rb
+++ app/models/estimate.rb
@@ -4,7 +4,7 @@ class Estimate < ActiveRecord::Base
   has_many :items, as: :itemeable
   belongs_to :receiver, class_name: 'User'
 
-  validates :summary, :bill_to, :bill_number, :date, presence: true
+  validates :summary, :to, :number, :date, presence: true
 
   accepts_nested_attributes_for :items, allow_destroy: true
 
