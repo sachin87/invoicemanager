f4c5d4fad49f1657655a322809a186b3caef0b34
Sachin Singh <singhsachin87@yahoo.com>
2014-09-08 16:39:11 +0200

    bills form updated

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,11 +1,22 @@
 class Bill < ActiveRecord::Base
 
+  attr_reader :receiver_tokens
+
   belongs_to :user
   has_many :items, as: :itemeable
-  belongs_to :receiver, class_name: 'User'
+  belongs_to :receiver, class_name: 'Client'
 
   validates :summary, :bill_to, :bill_number, :date, presence: true
+  validates :bill_number, uniqueness: true
 
   accepts_nested_attributes_for :items, allow_destroy: true
+
+  def receiver_tokens=(tokens)
+    self.receiver_id = Client.ids_from_tokens(tokens)
+  end
+
+  def receiver_fullname
+    receiver && receiver.full_name
+  end
 
 end
